9768327db28e8ed53d510b6dc1c74730399c4837
liuy58 <liuy58@mingyuanyun.com>
2020-08-26 15:48:14 +0800

    修改button名称

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,7 +3,7 @@
     <h3 class="jumbotron-heading">Search Github Users</h3>
     <div>
       <input type="text" placeholder="enter the name you search" v-model="searchName"/>
-      <button @click="search">Search</button>
+      <button @click="search">Search1</button>
     </div>
   </section>
 </template>
