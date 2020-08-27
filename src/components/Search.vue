1d4efa8720f1e7dd4b9ce70463b7d93c5aa7b921
liuy58 <liuy58@mingyuanyun.com>
2020-08-27 09:43:30 +0800

    rick

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,7 +3,7 @@
     <h3 class="jumbotron-heading">Search Github Users</h3>
     <div>
       <input type="text" placeholder="enter the name you search" v-model="searchName"/>
-      <button @click="search">Search1</button>
+      <button @click="search">Search</button>
     </div>
   </section>
 </template>
